9a2f676438b8387dd660e19e2232f1563f26eddc
hz90 <kaisyu.chou@gmail.com>
2021-08-28 19:10:41 +0900

    spa admin init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
+    <h1>{{ getMsg + age }}</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class About extends Vue {
+  private msg = '1test12xxxx';
+  private age = 10;
+  private get getMsg(): string {
+    return 'getmsg11' + this.msg;
+  }
+  private test(): void {
+    console.log('hello');
+  }
+}
+</script>
