a00527b4f3de124be3321ab20711bd7e47a5d126
hz90 <kaisyu.chou@gmail.com>
2021-09-18 18:11:14 +0900

    删除normalize

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import router from './router';
 import store from './store';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
-import 'normalize.css';
 import { BootstrapVue, BootstrapVueIcons, ToastPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
