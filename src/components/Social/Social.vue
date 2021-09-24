ec9dc0bbc38301a99b3eae43276a9a567dd935c5
hz90 <kaisyu.chou@gmail.com>
2021-09-24 22:10:33 +0900

    登录

--- src/components/Social/Social.vue
+++ src/components/Social/Social.vue
@@ -1,123 +1,94 @@
 <template lang="html">
-  <div id="logreg-forms">
-    <form class="form-signin">
-      <h1 class="h3 mb-3 font-weight-normal" style="text-align: center">
-        Sign in
-      </h1>
-      <div class="social-login">
-        <button class="btn facebook-btn social-btn" type="button">
-          <span><i class="fab fa-facebook-f"></i> Sign in with Facebook</span>
-        </button>
-        <button class="btn google-btn social-btn" type="button">
-          <span><i class="fab fa-google-plus-g"></i> Sign in with Google+</span>
-        </button>
+  <div class="login-wrap">
+    <div class="login-html">
+      <input id="tab-1" type="radio" name="tab" class="sign-in" checked /><label
+        for="tab-1"
+        class="tab"
+        >Sign In</label
+      >
+      <input id="tab-2" type="radio" name="tab" class="sign-up" /><label
+        for="tab-2"
+        class="tab"
+        >Sign Up</label
+      >
+      <div class="login-form">
+        <div class="sign-in-htm">
+          <div class="group">
+            <label for="user" class="label">Username</label>
+            <input id="user" type="text" class="input" required="" />
+          </div>
+          <div class="group">
+            <label for="pass" class="label">Password</label>
+            <input id="pass" type="password" class="input" required="" />
+          </div>
+          <div class="group">
+            <input id="check" type="checkbox" class="check" checked />
+            <label for="check"
+              ><span class="icon"></span> Keep me Signed in</label
+            >
+          </div>
+          <div class="group">
+            <input
+              type="button"
+              class="button"
+              @click="login()"
+              value="Sign In"
+            />
+          </div>
+          <div class="hr"></div>
+          <div class="foot-lnk">
+            <span>Forgot Password?</span>
+          </div>
+        </div>
+        <div class="sign-up-htm">
+          <div class="group">
+            <label for="user" class="label">Username</label>
+            <input v-model="username" id="user" type="text" class="input" />
+          </div>
+          <div class="group">
+            <label for="pass" class="label">Password</label>
+            <input
+              id="pass"
+              v-model="password"
+              type="password"
+              class="input"
+              data-type="password"
+            />
+          </div>
+          <div class="group">
+            <label for="pass" class="label">Repeat Password</label>
+            <input
+              id="pass"
+              type="password"
+              class="input"
+              data-type="password"
+            />
+          </div>
+          <div class="group">
+            <label for="pass" class="label">Email Address</label>
+            <input id="pass" type="text" class="input" />
+          </div>
+          <div class="group">
+            <input type="submit" class="button" value="Sign Up" />
+          </div>
+        </div>
       </div>
-      <p style="text-align: center">OR</p>
-      <input
-        type="email"
-        id="inputEmail"
-        class="form-control"
-        placeholder="Email address"
-        required=""
-        autofocus=""
-      />
-      <input
-        type="password"
-        id="inputPassword"
-        class="form-control"
-        placeholder="Password"
-        required=""
-      />
-
-      <button class="btn btn-success btn-block" type="submit">
-        <i class="fas fa-sign-in-alt"></i> Sign in
-      </button>
-      <a href="#" id="forgot_pswd">Forgot password?</a>
-      <hr />
-      <!-- <p>Don't have an account!</p>  -->
-      <button class="btn btn-primary btn-block" type="button" id="btn-signup">
-        <i class="fas fa-user-plus"></i> Sign up New Account
-      </button>
-    </form>
-
-    <form action="/reset/password/" class="form-reset">
-      <input
-        type="email"
-        id="resetEmail"
-        class="form-control"
-        placeholder="Email address"
-        required=""
-        autofocus=""
-      />
-      <button class="btn btn-primary btn-block" type="submit">
-        Reset Password
-      </button>
-      <a href="#" id="cancel_reset"><i class="fas fa-angle-left"></i> Back</a>
-    </form>
-
-    <form action="/signup/" class="form-signup">
-      <div class="social-login">
-        <button class="btn facebook-btn social-btn" type="button">
-          <span><i class="fab fa-facebook-f"></i> Sign up with Facebook</span>
-        </button>
-      </div>
-      <div class="social-login">
-        <button class="btn google-btn social-btn" type="button">
-          <span><i class="fab fa-google-plus-g"></i> Sign up with Google+</span>
-        </button>
-      </div>
-
-      <p style="text-align: center">OR</p>
-
-      <input
-        type="text"
-        id="user-name"
-        class="form-control"
-        placeholder="Full name"
-        required=""
-        autofocus=""
-      />
-      <input
-        type="email"
-        id="user-email"
-        class="form-control"
-        placeholder="Email address"
-        required
-        autofocus=""
-      />
-      <input
-        type="password"
-        id="user-pass"
-        class="form-control"
-        placeholder="Password"
-        required
-        autofocus=""
-      />
-      <input
-        type="password"
-        id="user-repeatpass"
-        class="form-control"
-        placeholder="Repeat Password"
-        required
-        autofocus=""
-      />
-
-      <button class="btn btn-primary btn-block" type="submit">
-        <i class="fas fa-user-plus"></i> Sign Up
-      </button>
-      <a href="#" id="cancel_signup"><i class="fas fa-angle-left"></i> Back</a>
-    </form>
-    <br />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { loginModule } from '@/store/modules/login-store';
+import { publicKeyStoreModule } from '@/store/modules/publickey-store';
+import { loginStoreVo } from '@/store/vo/login-store-vo';
 @Component({
   name: 'Social',
   components: {},
 })
 export default class Social extends Vue {
+  private username = '';
+  private password = '';
   private beforeCreate() {
     // this.$store.commit('showMiniMusic', false);
   }
@@ -127,168 +98,163 @@ export default class Social extends Vue {
   private mounted() {
     // this.$store.commit('changeLinkBorderIndex', 3);
   }
-
-  private date = '';
-  private volume = '';
-  private imgUrl = '';
-  private picInfo = '';
-  private forward = '';
-  private wordsInfo = '';
-  private index = 0;
-  private isLoading = false;
-  private isShowMask = false;
-  private isShowControl = false;
-
-  private getOne(index = 0): void {
-    if (index > 9) {
-      this.index = 9;
-      return;
-    }
-    if (index < 0) {
-      this.index = 0;
-      return;
-    }
-    this.isLoading = true;
-    // this.axios
-    //   .get('/api/one/' + index)
-    //   .then((res) => res.data.data.content_list[0])
-    //   .then((one) => {
-    //     this.date = one.post_date.split(' ')[0].replace(/\-/g, ' / ');
-    //     this.volume = one.volume;
-    //     this.imgUrl = one.img_url;
-    //     this.picInfo = one.pic_info;
-    //     this.forward = one.forward;
-    //     this.wordsInfo = one.words_info;
-    //     this.isLoading = false;
-    //     this.isShowMask = false;
-    //     setTimeout(() => {
-    //       this.isShowControl = false;
-    //     }, 250);
-    //   });
+  public async login(): Promise<void> {
+    await publicKeyStoreModule.exeGetPublicKeyApi();
+    console.log('publickey:' + publicKeyStoreModule.getMypublicKey);
+    let loginStoreVo: loginStoreVo = {
+      username: this.username,
+      password: this.password,
+    };
+    await loginModule.executeLoginApi(loginStoreVo);
   }
 }
 </script>
 
 <style lang="scss" scoped>
-/* sign in FORM */
-#logreg-forms {
-  width: 412px;
-  margin: 10vh auto;
-  background-color: #f3f3f3;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
-}
-#logreg-forms form {
+.login-wrap {
   width: 100%;
-  max-width: 410px;
-  padding: 15px;
   margin: auto;
-}
-#logreg-forms .form-control {
+  max-width: 525px;
+  min-height: 650px;
   position: relative;
-  box-sizing: border-box;
-  height: auto;
-  padding: 10px;
-  font-size: 16px;
+  //background: url(https://raw.githubusercontent.com/khadkamhn/day-01-login-form/master/img/bg.jpg) no-repeat center;
+  box-shadow: 0 12px 15px 0 rgba(0, 0, 0, 0.24),
+    0 17px 50px 0 rgba(0, 0, 0, 0.19);
 }
-#logreg-forms .form-control:focus {
-  z-index: 2;
+.login-html {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  padding: 90px 70px 50px 70px;
+  background: rgba(40, 57, 101, 0.9);
 }
-#logreg-forms .form-signin input[type='email'] {
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
+.login-html .sign-in-htm,
+.login-html .sign-up-htm {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  position: absolute;
+  transform: rotateY(180deg);
+  backface-visibility: hidden;
+  transition: all 0.4s linear;
 }
-#logreg-forms .form-signin input[type='password'] {
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-}
-
-#logreg-forms .social-login {
-  width: 390px;
-  margin: 0 auto;
-  margin-bottom: 14px;
-}
-#logreg-forms .social-btn {
-  font-weight: 100;
-  color: white;
-  width: 190px;
-  font-size: 0.9rem;
-}
-
-#logreg-forms a {
-  display: block;
-  padding-top: 10px;
-  color: lightseagreen;
-}
-
-#logreg-form .lines {
-  width: 200px;
-  border: 1px solid red;
-}
-
-#logreg-forms button[type='submit'] {
-  margin-top: 10px;
-}
-
-#logreg-forms .facebook-btn {
-  background-color: #3c589c;
-}
-
-#logreg-forms .google-btn {
-  background-color: #df4b3b;
-}
-
-#logreg-forms .form-reset,
-#logreg-forms .form-signup {
+.login-html .sign-in,
+.login-html .sign-up,
+.login-form .group .check {
   display: none;
 }
-
-#logreg-forms .form-signup .social-btn {
-  width: 210px;
+.login-html .tab,
+.login-form .group .label,
+.login-form .group .button {
+  text-transform: uppercase;
+}
+.login-html .tab {
+  font-size: 22px;
+  margin-right: 15px;
+  padding-bottom: 5px;
+  margin: 0 15px 10px 0;
+  display: inline-block;
+  border-bottom: 2px solid transparent;
+}
+.login-html .sign-in:checked + .tab,
+.login-html .sign-up:checked + .tab {
+  color: #fff;
+  border-color: #1161ee;
+}
+.login-form {
+  min-height: 345px;
+  position: relative;
+  perspective: 1000px;
+  transform-style: preserve-3d;
+}
+.login-form .group {
+  margin-bottom: 15px;
+}
+.login-form .group .label,
+.login-form .group .input,
+.login-form .group .button {
+  width: 100%;
+  color: #fff;
+  display: block;
+}
+.login-form .group .input,
+.login-form .group .button {
+  border: none;
+  padding: 15px 20px;
+  border-radius: 25px;
+  background: rgba(255, 255, 255, 0.1);
+}
+.login-form .group input[data-type='password'] {
+  text-security: circle;
+  -webkit-text-security: circle;
+}
+.login-form .group .label {
+  color: #aaa;
+  font-size: 12px;
+}
+.login-form .group .button {
+  background: #1161ee;
+}
+.login-form .group label .icon {
+  width: 15px;
+  height: 15px;
+  border-radius: 2px;
+  position: relative;
+  display: inline-block;
+  background: rgba(255, 255, 255, 0.1);
+}
+.login-form .group label .icon:before,
+.login-form .group label .icon:after {
+  content: '';
+  width: 10px;
+  height: 2px;
+  background: #fff;
+  position: absolute;
+  transition: all 0.2s ease-in-out 0s;
+}
+.login-form .group label .icon:before {
+  left: 3px;
+  width: 5px;
+  bottom: 6px;
+  transform: scale(0) rotate(0);
+}
+.login-form .group label .icon:after {
+  top: 6px;
+  right: 0;
+  transform: scale(0) rotate(0);
+}
+.login-form .group .check:checked + label {
+  color: #fff;
+}
+.login-form .group .check:checked + label .icon {
+  background: #1161ee;
+}
+.login-form .group .check:checked + label .icon:before {
+  transform: scale(1) rotate(45deg);
+}
+.login-form .group .check:checked + label .icon:after {
+  transform: scale(1) rotate(-45deg);
+}
+.login-html
+  .sign-in:checked
+  + .tab
+  + .sign-up
+  + .tab
+  + .login-form
+  .sign-in-htm {
+  transform: rotate(0);
+}
+.login-html .sign-up:checked + .tab + .login-form .sign-up-htm {
+  transform: rotate(0);
 }
 
-#logreg-forms .form-signup input {
-  margin-bottom: 2px;
+.hr {
+  height: 2px;
+  margin: 60px 0 50px 0;
+  background: rgba(255, 255, 255, 0.2);
 }
-
-.form-signup .social-login {
-  width: 210px !important;
-  margin: 0 auto;
-}
-
-/* Mobile */
-
-@media screen and (max-width: 600px) {
-  #logreg-forms {
-    width: 300px;
-    height: 100%;
-  }
-
-  #logreg-forms .social-login {
-    width: 200px;
-    margin: 0 auto;
-    margin-bottom: 10px;
-  }
-  #logreg-forms .social-btn {
-    font-size: 1.3rem;
-    font-weight: 100;
-    color: white;
-    width: 200px;
-    height: 56px;
-  }
-  #logreg-forms .social-btn:nth-child(1) {
-    margin-bottom: 5px;
-  }
-  #logreg-forms .social-btn span {
-    display: none;
-  }
-  #logreg-forms .facebook-btn:after {
-    content: 'Facebook';
-  }
-
-  #logreg-forms .google-btn:after {
-    content: 'Google+';
-  }
+.foot-lnk {
+  text-align: center;
 }
 </style>
